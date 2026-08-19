3ae00ba1cf40b064b470a56b86e670bbdb995707
imya <gucciass68@gmail.com>
2026-08-19 10:52:21 +0000

    created a tsconfig.build.json

---
+++ test/index.test.ts
@@ -0,0 +1,8 @@
+import {describe, it, expect} from 'vitest'
+import {readFileSync} from 'node:fs'
+describe('Parse excel to json',()=>{
+it('Parse un fichier excel valide', async()=>{
+
+    
+})
+})
