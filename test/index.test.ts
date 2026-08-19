e192ec4b2031f5df302a034ed29a59b0191846a3
imya <gucciass68@gmail.com>
2026-08-19 15:51:10 +0000

    test with vitest set up

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,8 +1,51 @@
 import {describe, it, expect} from 'vitest'
-import {readFileSync} from 'node:fs'
+import * as fs from 'node:fs'
+import { parseExcelToJson } from '../src'
+import path from 'node:path'
+
 describe('Parse excel to json',()=>{
 it('Parse un fichier excel valide', async()=>{
+    const filePath = path.join(import.meta.dirname,'fixtures', 'test_employes.xlsx')
+    const data = fs.readFileSync(filePath)
+   // const biteArray = new Uint8Array(data)
+    const file = new File([data],"employes.xlsx")
+    expect(await parseExcelToJson(file)).toEqual([
+  {
+    "nom": "Mya Aidara",
+    "poste": "Développeuse",
+    "salaire": 450000,
+    "dateEmbauche": 45000,
+    "actif": true
+  },
+  {
+    "nom": "Awa Ndiaye",
+    "poste": "Comptable",
+    "salaire": 380000,
+    "dateEmbauche": 44440,
+    "actif": true
+  },
+  {
+    "nom": "Ibrahima Sow",
+    "poste": "Chef de projet",
+    "salaire": 620000,
+    "dateEmbauche": 43485,
+    "actif": false
+  },
+  {
+    "nom": "Fatou Diop",
+    "poste": "RH",
+    "salaire": 410000,
+    "dateEmbauche": 44722,
+    "actif": true
+  }
+])
+})
+})
 
-    
-})
-})
+// describe('Parse excel to json INVALID',()=>{
+//     it('Parse un fichier invalide', async()=>{
+//         const file = new File([new Uint16Array([1,2,3,4,5,6,7,8,9,10])], "file.txt")
+//        await expect(parseExcelToJson(file)).rejects.toThrow("No sheet in this file")
+//     })
+// })
+
